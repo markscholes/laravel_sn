4c1b45187371bdcc933b751b99a11706dc2b6bf5
Mark Scholes <markscholes85@gmail.com>
2017-01-26 10:52:17 +0000

    form validation
    Added form validation and error message display options

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,13 @@ class UserController extends Controller{
   }
   //sign up method
 public function postSignUp(Request $request){
+  $this->validate($request, [
+    'email' => 'required|unique:users', //specify the rules that are in place for field 'email', the second email here is a rule and resticting submissions, ' | ' seperates each rule, second rule is that it must be unique entry : in the users table
+    'first_name' => 'required|max:100', //max specifies the maximum number of characters required
+    'last_name' => 'required|max:100',
+    'password' => 'required|min: 4',
+  ]);
+  //one of laravel's predefined functions to help with security
   $email = $request['email'];
   $password = bcrypt($request['password']);
   $first_name = $request['first_name'];
@@ -27,11 +34,16 @@ public function postSignUp(Request $request){
 
   $user->save();
 
+  Auth::login($user);
   return redirect()->route('dashboard');
 }
 
   //sign in menthod
   public function postSignIn(Request $request){
+    $this->validate($request, [
+      'email' => 'required',
+      'password' => 'required',
+    ]);
     if(Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
       return redirect()->route('dashboard');
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,23 +5,34 @@ Sign In
 @endsection
 
 @section('content')
+  @if(count($errors) > 0)
+  <div class="row">
+    <div class="col-md-6">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{$error}}</li>
+        @endforeach
+      </ul>
+    </div>
+  </div>
+  @endif
   <div class="row">
     <div class="col-md-6">
       <h4>Register</h4>
       <form action="{{ route('signup') }}" method="post">
-        <div class="form-group">
+        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
           <label for="email">Email:</label>
-          <input type="email" name="email" id="email" class="form-control" />
+          <input type="email" name="email" id="email" class="form-control" value="{{ Request::old('email') }}" /> <!-- This is to make sure that if one of the fields is incorrect and the form reloads the user is not required to enter all data again-->
         </div>
-        <div class="form-group">
+        <div class="form-group form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
           <label for="first_name">First Name:</label>
-          <input type="text" name="first_name" id="first_name" class="form-control" />
+          <input type="text" name="first_name" id="first_name" class="form-control" value="{{ Request::old('first_name') }}" />
         </div>
-        <div class="form-group">
+        <div class="form-group form-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
           <label for="last_name">Last Name:</label>
-          <input type="text" name="last_name" id="last_name" class="form-control" />
+          <input type="text" name="last_name" id="last_name" class="form-control" value="{{ Request::old('last_name') }}" />
         </div>
-        <div class="form-group">
+        <div class="form-group form-group {{ $errors->has('password') ? 'has-error' : '' }}">
           <label for="password">Password:</label>
           <input type="password" name="password" id ="password" class="form-control" />
         </div>
@@ -35,11 +46,11 @@ Sign In
     <div class="col-md-6">
       <h4>Log In</h4>
       <form action="{{ route('signin') }}" method="post">
-        <div class="form-group">
+        <div class="form-group form-group form-group {{ $errors->has('email') ? 'has-error' : '' }}">
           <label for="email">Email:</label>
-          <input type="email" name="email" id="email" class="form-control" />
+          <input type="email" name="email" id="email" class="form-control" value="{{ Request::old('email') }}"  />
         </div>
-        <div class="form-group">
+        <div class="form-group form-group form-group {{ $errors->has('password') ? 'has-error' : '' }}">
           <label for="password">Password:</label>
           <input type="password" name="password" id ="password" class="form-control" />
         </div>
